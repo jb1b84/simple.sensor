c6d3867e9652383aca98b480201bd002e24d4e87
Jason Brown <jason@ant-bytes.com>
2021-07-17 12:47:27 -0500

    updating device script

--- device/rpi_thermocouple.py
+++ device/rpi_thermocouple.py
@@ -1,16 +1,26 @@
 import time
+from datetime import datetime
+import requests
 import board
 import digitalio
 import adafruit_max31855
 
 # Created from https://learn.adafruit.com/thermocouple/python-circuitpython
 
+"""
+TODO:
+    * mock board specific stuff for local dev
+    * logging
+    * env file with ability to change endpoint
+    * draw schemas from something more elegant
+"""
+
 spi = board.SPI()
 cs = digitalio.DigitalInOut(board.D5)
 max31855 = adafruit_max31855.MAX31855(spi, cs)
 
 
-def getserial():
+def get_serial():
     # Extract serial from cpuinfo file
     cpuserial = "0000000000000000"
     try:
@@ -25,12 +35,69 @@ def getserial():
     return cpuserial
 
 
-deviceId = getserial()
+device_id = get_serial()
 
-print('Starting up device: {}'.format(deviceId))
+print('Starting up device: {}'.format(device_id))
 
-while True:
+
+def get_temp():
     tempC = max31855.temperature
-    tempF = tempC * 9 / 5 + 32
-    print('Temperature: {} degrees C'.format(tempF))
-    time.sleep(2.0)
+    return tempC * 9 / 5 + 32
+
+
+def get_session_data():
+    username = input("Username: \n")
+    label = input("Session label: \n")
+
+    return username, label
+
+
+def format_reading(reading, device_time, reading_type="temperature", reading_label="internal temp", unit_of_measure="F"):
+    return {
+        "type": reading_type,
+        "label": reading_label,
+        "reading": str(reading),
+        "UoM": unit_of_measure,
+        "recorded_at": device_time
+    }
+
+
+def send_readings(readings, username, session_label, device_id="na"):
+    # move to env
+    endpoint = "https://us-central1-simple-sensor.cloudfunctions.net/event"
+    device_time = datetime.now().strftime("%c")
+
+    formatted_readings = [format_reading(
+        reading, device_time) for reading in readings]
+
+    payload = {
+        "schema": "streaming_sensor",
+        "version": "0.1",
+        "device_time": device_time,
+        "username": username,
+        "device_name": device_id,
+        "readings": formatted_readings,
+        "session": {
+            "label": session_label
+        }
+    }
+
+    print(payload)
+
+    return
+
+
+def main_loop():
+    username, session_label = get_session_data()
+    print('Now collecting data for user {} and session {}'.format(
+        username, session_label))
+
+    while True:
+        tempF = get_temp()
+        send_readings([tempF], username, session_label)
+
+        time.sleep(5.0)
+
+
+if __name__ == "__main__":
+    main_loop()
